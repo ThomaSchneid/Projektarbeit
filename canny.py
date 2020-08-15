f228d51620f4b5e9f342730cc79cf6b1a8737067
Thomas <ts@papershift.com>
2020-08-15 22:06:58 +0200

    Clean code

--- canny.py
+++ canny.py
@@ -2,16 +2,12 @@ import cv2
 import numpy as np
 from scipy import ndimage
 
-# NOISE REDUCTION
-# Gaussian 5 x 5 Kernel
-
 def gaussian_kernel(size, sigma=1.8):
     size = int(size) // 2
     x, y = np.mgrid[-size:size + 1, -size:size + 1]
     normal = 1 / (2.0 * np.pi * sigma ** 2)
     g = np.exp(-((x ** 2 + y ** 2) / (2.0 * sigma ** 2))) * normal
     return g
-
 
 def gaussian_blur():
     img = cv2.imread("Bilder/hochschule.png", 0)
@@ -20,9 +16,6 @@ def gaussian_blur():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     return blur
-
-# GRADIENT CALCULATION
-# Sobel Filter
 
 def sobel_filter(img):
     Gx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
@@ -38,15 +31,7 @@ def sobel_filter(img):
     cv2.imshow("Image after sobel", G)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
     return G, theta
-
-
-# NON MAXIMUM SUPPRESSION
-# Create a matrix initialized to 0 of the same size of the original gradient intesity matrix
-# Identify the edge direction based on the angle  value from the angle matrix
-# Check if the pixel in the same direction has a higher intensity than the pixel that is currently processed
-# Return the image processed with the non-max suppression algorithm
 
 def non_max_suppression(img, D):
     M, N = img.shape
@@ -90,9 +75,6 @@ def non_max_suppression(img, D):
     cv2.destroyAllWindows()
     return Z
 
-
-# DOUBLE THRESHOLD
-
 def double_threshold(img, lowRatio=0.05, highRatio=0.09):
     highThreshold = img.max() * highRatio
     lowThreshold = highThreshold * lowRatio
@@ -115,13 +97,7 @@ def double_threshold(img, lowRatio=0.05, highRatio=0.09):
     cv2.imshow("Image after double threshold", res)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
     return res, weak, strong
-
-
-# EDGE TRACKING BY HYSTERESIS
-# Transform weak pixels in strong pixels
-
 
 def hysteresis(img, weak, strong=255):
     M, N = img.shape
@@ -143,7 +119,6 @@ def hysteresis(img, weak, strong=255):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     return img
-
 
 def test():
     blur = gaussian_blur()
