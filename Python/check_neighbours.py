fdf711f61a209aeccc62c0adaea1b1a708a434e1
Thomas <ts@papershift.com>
2020-09-09 00:33:16 +0200

    add check neighbours

--- Python/check_neighbours.py
+++ Python/check_neighbours.py
@@ -1,74 +1,79 @@
-def top(pos, img):
+def top(pos, img, diff):
     list = [-1,0,1]
     sum_top = 0
-    for a in list:
-        sum_top += img[pos[0] - 1][pos[1] + a]
+    for i in range(1, diff + 1):
+        for j in list:
+            sum_top += img[pos[0] - i][pos[1] + j]
 
-    return sum_top
+    return sum_top/(3 * diff)
 
-def bottom(pos, img):
+def bottom(pos, img, diff):
     list = [-1, 0, 1]
     sum_bottom = 0
-    for a in list:
-        sum_bottom += img[pos[0] + 1][pos[1] + a]
+    for i in range(1, diff + 1):
+        for j in list:
+            sum_bottom += img[pos[0] + i][pos[1] + j]
 
-    return sum_bottom
+    return sum_bottom/(3 * diff)
 
-def left(pos, img):
+def left(pos, img, diff):
     list = [-1, 0, 1]
     sum_left = 0
-    for a in list:
-        sum_left += img[pos[0] + a][pos[1] - 1]
+    for j in range(1, diff + 1):
+        for i in list:
+            sum_left += img[pos[0] + i][pos[1] - j]
 
-    return sum_left
+    return sum_left/(3 * diff)
 
-def right(pos, img):
+def right(pos, img, diff):
     list = [-1, 0, 1]
     sum_right = 0
-    for a in list:
-        sum_right += img[pos[0] + a][pos[1] + 1]
+    for j in range(1, diff + 1):
+        for i in list:
+            sum_right += img[pos[0] + i][pos[1] + j]
 
-    return sum_right
+    return sum_right/(3 * diff)
 
-def diagonal_top_left(pos, img):
-    list = [-1, 0]
-    sum_diagonal_top_left = 0
-    for a in list:
-        sum_diagonal_top_left += img[pos[0] + a][pos[1] - 1]
+def top_left(pos, img, diff):
+    sum_top_left = 0
+    for j in range(0, diff + 1):
+        for i in range(0, diff + 1):
+            sum_top_left += img[pos[0] - i][pos[1] - j]
 
-    sum_diagonal_top_left += img[pos[0] - 1][pos[1]]
+    sum_top_left -= img[pos]
 
-    return sum_diagonal_top_left
+    return sum_top_left/((3 * diff + 1) - 1)
 
-def diagonal_top_right(pos, img):
-    list = [-1, 0]
-    sum_diagonal_top_right = 0
-    for a in list:
-        sum_diagonal_top_right += img[pos[0] + a][pos[1] + 1]
 
-    sum_diagonal_top_right += img[pos[0] - 1][pos[1]]
+def top_right(pos, img, diff):
+    sum_top_right = 0
+    for j in range(0, diff + 1):
+        for i in range(0, diff + 1):
+            sum_top_right += img[pos[0] - i][pos[1] + j]
 
-    return sum_diagonal_top_right
+    sum_top_right -= img[pos]
 
-def diagonal_bottom_left(pos, img):
-    list = [0, 1]
-    sum_diagonal_bottom_left = 0
-    for a in list:
-        sum_diagonal_bottom_left += img[pos[0] + a][pos[1] - 1]
+    return sum_top_right/((3 * diff + 1) - 1)
 
-    sum_diagonal_bottom_left += img[pos[0] + 1][pos[1]]
+def bottom_left(pos, img, diff):
+    sum_bottom_left = 0
+    for j in range(0, diff + 1):
+        for i in range(0, diff + 1):
+            sum_bottom_left += img[pos[0] + i][pos[1] - j]
 
-    return sum_diagonal_bottom_left
+    sum_bottom_left -= img[pos]
 
-def diagonal_bottom_right(pos, img):
-    list = [0, 1]
-    sum_diagonal_bottom_right = 0
-    for a in list:
-        sum_diagonal_bottom_right += img[pos[0] + a][pos[1] + 1]
+    return sum_bottom_left/((3 * diff + 1) - 1)
 
-    sum_diagonal_bottom_right += img[pos[0] + 1][pos[1]]
+def bottom_right(pos, img, diff):
+    sum_bottom_right = 0
+    for j in range(0, diff + 1):
+        for i in range(0, diff + 1):
+            sum_bottom_right += img[pos[0] + i][pos[1] + j]
 
-    return sum_diagonal_bottom_right
+    sum_bottom_right -= img[pos]
+
+    return sum_bottom_right/((3 * diff + 1) - 1)
 
 def value_top_bottom(pos, img):
     if img[pos[0] - 1][pos[1]] > img[pos[0] + 1][pos[1]]:
--- spirale.py
+++ spirale.py
@@ -1,7 +1,9 @@
+from Python.check_neighbours import *
 import cv2
 from scipy import ndimage
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 def load_image():
     image = cv2.imread("Bilder/rauschen.png", 0)
@@ -13,28 +15,41 @@ def create_whitescreen(image):
     return image
 
 def spiral(img, diff):
+    start = time.time()
     x_length = img.shape[1]
     y_length = img.shape[0]
-    px = 0
+    px = 1
+    check_diff = diff
+    if diff > 3:
+        check_diff = 3
 
     while (y_length - 2 * px) >= diff and (x_length - 2 * px) >= diff:
         px += diff
         # links nach rechts oben
         for j in range(px, x_length - px):
-            img[px, j] = 0
+            # img[px, j] = 0
+            check_pixel((px, j), img, check_diff)
         # oben nach unten hinten
         for i in range(px, y_length - px):
-            img[i, x_length - px] = 0
+            # img[i, x_length - px] = 0
+            check_pixel((i, x_length - px), img, check_diff)
         # links nach rechts unten
         for j in range(px, x_length - px):
-            img[y_length - px, j] = 0
+            # img[y_length - px, j] = 0
+            check_pixel((y_length - px, j), img, check_diff)
         # oben nach unten vorne
         for i in range(px, y_length - px):
-            img[i, px] = 0
+            # img[i, px] = 0
+            check_pixel((i, px), img, check_diff)
+    end = time.time()
+    print(end - start)
     return img
 
-def check_pixel():
+def check_pixel(pos, image, diff):
+    falling_diagonal = round(top_left(pos, image, diff) / bottom_right(pos, image, diff), 4)
+    top_bottom = round(top(pos, image, diff) / bottom(pos, image, diff), 4)
+    raising_diagonal = round(top_right(pos, image, diff) / bottom_left(pos, image, diff), 4)
+    right_left = round(right(pos, image, diff) / left(pos, image, diff), 4)
 
+    return top_bottom, raising_diagonal, right_left, falling_diagonal
 
-
-    return 0
