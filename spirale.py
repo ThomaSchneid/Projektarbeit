3e11f592b59fff0db7b3bf4bcfa8d9166c53474e
Thomas <ts@papershift.com>
2020-09-15 00:12:00 +0200

    start edge tracking

--- spirale.py
+++ spirale.py
@@ -19,6 +19,7 @@ def spiral(img, diff):
     x_length = img.shape[1]
     y_length = img.shape[0]
     px = 1
+    possible_directions = [0,1,2,3]
     check_diff = diff
     if diff > 3:
         check_diff = 3
@@ -27,16 +28,22 @@ def spiral(img, diff):
         px += diff
         # links nach rechts oben
         for j in range(px, x_length - px):
+            direction = check_pixel((px, j), img, check_diff)
             whitescreen[px, j] = check_pixel((px, j), img, check_diff)
         # oben nach unten hinten
         for i in range(px, y_length - px):
+            direction = check_pixel((i, x_length - px), img, check_diff)
             whitescreen[i, x_length - px] = check_pixel((i, x_length - px), img, check_diff)
         # links nach rechts unten
         for j in range(px, x_length - px):
+            direction = check_pixel((y_length - px, j), img, check_diff)
             whitescreen[y_length - px, j] = check_pixel((y_length - px, j), img, check_diff)
         # oben nach unten vorne
         for i in range(px, y_length - px):
+            direction = check_pixel((i, px), img, check_diff)
             whitescreen[i, px] = check_pixel((i, px), img, check_diff)
+            if direction in possible_directions:
+                check_next_pixel_up_down(whitescreen, img, i, px, check_diff, direction)
     end = time.time()
     print(end - start)
     return whitescreen
@@ -44,18 +51,21 @@ def spiral(img, diff):
 def check_pixel(pos, image, diff):
     check_array = []
 
-    check_array.append(round(top_left(pos, image, diff) / bottom_right(pos, image, diff), 4))
-    check_array.append(round(top(pos, image, diff) / bottom(pos, image, diff), 4))
-    check_array.append(round(top_right(pos, image, diff) / bottom_left(pos, image, diff), 4))
-    check_array.append(round(right(pos, image, diff) / left(pos, image, diff), 4))
+    check_array.append(top_left(pos, image, diff) - bottom_right(pos, image, diff))
+    check_array.append(top(pos, image, diff) - bottom(pos, image, diff))
+    check_array.append(top_right(pos, image, diff) - bottom_left(pos, image, diff))
+    check_array.append(right(pos, image, diff) - left(pos, image, diff))
 
-    for idx, val in enumerate(check_array):
-        if val > 1:
-            check_array[idx] = round(1/val, 4)
-
-    maxim = max(check_array)
-    if maxim > 0.4:
+    maxim = max(check_array, key = abs)
+    if abs(maxim) > 20:
         direction = check_array.index(maxim)
+        # Wenn es keine Kante ist wird dem Pixel der Wert 255 zugeordnet
     else:
         direction = 255
     return direction
+
+
+def check_next_pixelup_down(whitescreen, img, i, px, check_diff, direction):
+    if direction == 0:
+        check_pixel((i + 1, px - 1))
+    return true
