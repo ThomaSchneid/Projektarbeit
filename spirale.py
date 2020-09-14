b35f02bf2863174f5ad0f2e72af37c0fb97f4507
Thomas <ts@papershift.com>
2020-09-14 21:58:44 +0200

    add files to git

--- spirale.py
+++ spirale.py
@@ -6,16 +6,16 @@ import matplotlib.pyplot as plt
 import time
 
 def load_image():
-    image = cv2.imread("Bilder/rauschen.png", 0)
+    image = cv2.imread("Bilder/Camera_obscura.jpg", 0)
     return image
 
 def create_whitescreen(image):
-    for i in range(image.shape[0]):
-        image[i] = 255
-    return image
+    whitescreen = np.full((image.shape), 255)
+    return whitescreen
 
 def spiral(img, diff):
     start = time.time()
+    whitescreen = create_whitescreen(img)
     x_length = img.shape[1]
     y_length = img.shape[0]
     px = 1
@@ -27,29 +27,35 @@ def spiral(img, diff):
         px += diff
         # links nach rechts oben
         for j in range(px, x_length - px):
-            # img[px, j] = 0
-            check_pixel((px, j), img, check_diff)
+            whitescreen[px, j] = check_pixel((px, j), img, check_diff)
         # oben nach unten hinten
         for i in range(px, y_length - px):
-            # img[i, x_length - px] = 0
-            check_pixel((i, x_length - px), img, check_diff)
+            whitescreen[i, x_length - px] = check_pixel((i, x_length - px), img, check_diff)
         # links nach rechts unten
         for j in range(px, x_length - px):
-            # img[y_length - px, j] = 0
-            check_pixel((y_length - px, j), img, check_diff)
+            whitescreen[y_length - px, j] = check_pixel((y_length - px, j), img, check_diff)
         # oben nach unten vorne
         for i in range(px, y_length - px):
-            # img[i, px] = 0
-            check_pixel((i, px), img, check_diff)
+            whitescreen[i, px] = check_pixel((i, px), img, check_diff)
     end = time.time()
     print(end - start)
-    return img
+    return whitescreen
 
 def check_pixel(pos, image, diff):
-    falling_diagonal = round(top_left(pos, image, diff) / bottom_right(pos, image, diff), 4)
-    top_bottom = round(top(pos, image, diff) / bottom(pos, image, diff), 4)
-    raising_diagonal = round(top_right(pos, image, diff) / bottom_left(pos, image, diff), 4)
-    right_left = round(right(pos, image, diff) / left(pos, image, diff), 4)
+    check_array = []
 
-    return top_bottom, raising_diagonal, right_left, falling_diagonal
+    check_array.append(round(top_left(pos, image, diff) / bottom_right(pos, image, diff), 4))
+    check_array.append(round(top(pos, image, diff) / bottom(pos, image, diff), 4))
+    check_array.append(round(top_right(pos, image, diff) / bottom_left(pos, image, diff), 4))
+    check_array.append(round(right(pos, image, diff) / left(pos, image, diff), 4))
 
+    for idx, val in enumerate(check_array):
+        if val > 1:
+            check_array[idx] = round(1/val, 4)
+
+    maxim = max(check_array)
+    if maxim > 0.4:
+        direction = check_array.index(maxim)
+    else:
+        direction = 255
+    return direction
