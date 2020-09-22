bf1f66446eb22543a04670695d22f40f3fe446d3
Thomas <ts@papershift.com>
2020-09-22 14:01:54 +0200

    Update Doku

--- helper.py
+++ helper.py
@@ -7,3 +7,13 @@ def load_image():
 def create_whitescreen(image):
     whitescreen = np.full((image.shape), 254)
     return whitescreen
+
+def subplots(rows, columns, images):
+    add = 1
+    position = rows * 100 + columns * 10 + add
+    for img in images:
+        plt.subplot(position), plt.imshow(img, cmap='gray')
+        plt.title('Bild 1'), plt.xticks([]), plt.yticks([])
+        add += 1
+
+    plt.show()
